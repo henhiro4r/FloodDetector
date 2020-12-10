2a36f0d522027653a3c54c74587365eedffeb1bc
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2020-12-10 08:43:32 +0700

    Added firebase dependency
    - Added firebase realtimedatabase dependency
    - Added simple read / write request

--- app/src/main/java/com/example/flooddetector/MainActivity.java
+++ app/src/main/java/com/example/flooddetector/MainActivity.java
@@ -1,14 +1,42 @@
 package com.example.flooddetector;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("test");
+
+        reference.setValue("Hello World");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w(TAG, "onCancelled: ", error.toException());
+            }
+        });
     }
 }
